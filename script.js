82a3b4321fa6ce52858b00a85756717ac6c94984
jvondzerza <jvondzerza@googlemail.com>
2021-10-14 22:11:52 +0200

    initial styling

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 (function() {
 
-    let input = document.getElementById("pokemon-search");
+    let evoDiv = document.getElementById("evo-row");
+    let input = document.getElementById("poke-search");
     let searchButton = document.getElementById("search-button");
     let id = document.getElementById("id");
     let name = document.getElementById("name");
@@ -11,13 +12,24 @@
     let randomMoves = ["", "", "", ""];
     let fetchId = 1;
 
+    function inputSanitization() {
+        if (input.value.includes(" ")) {
+            input.value = input.value.replace(" ", "-");
+        }
+        if (input.value.includes(".")) {
+            input.value = input.value.replace(".", "");
+        }
+    }
+
     searchButton.addEventListener("click", function() {
+        inputSanitization();
         fetchId = input.value;
         fetchPokemon();
     })
 
     input.addEventListener("keypress", function (e){
         if (e.key === 'Enter') {
+            inputSanitization();
             fetchId = input.value;
             fetchPokemon();
         }
@@ -67,6 +79,7 @@
                 }
                 assignMoves();
                 fetchId = parseInt(data.id);
+                fetchEvolutions();
                 randomMoves = ["", "", "", ""];
             })
     }
@@ -74,21 +87,70 @@
     function assignMoves() {
         moves.innerHTML = "";
         randomMoves.forEach(randomMove => {
-            let move = document.createElement("li");
+            let move = document.createElement("p");
             move.innerHTML = randomMove;
             moves.appendChild(move);
         })
     }
 
-    fetchPokemon();
-
     function fetchEvolutions() {
+        let evolutions = [];
         fetch(`https://pokeapi.co/api/v2/pokemon-species/${fetchId}/`)
             .then(response => response.json())
             .then(data => {
-                console.log(data.evolution_chain.url)
+                fetch(data.evolution_chain.url)
+                    .then(response => response.json())
+                    .then(data => {
+                        let evolutionDetails = data.chain;
+                        console.log(evolutionDetails);
+                        function deetsToDom(pixelSize) {
+                            evoDiv.innerHTML = "";
+                            evolutions.forEach(evolution => {
+                                if (evolution === "wormadam") {
+                                    evolution = "wormadam-plant";
+                                }
+                                fetch(`https://pokeapi.co/api/v2/pokemon/${evolution}`)
+                                    .then(response => response.json())
+                                    .then(data => {
+                                        let figure = document.createElement("figure");
+                                        let evolutionChainImg = document.createElement("img");
+                                        figure.setAttribute("class", "evo-fig");
+                                        evolutionChainImg.setAttribute("class", "evo-img");
+                                        evolutionChainImg.src = data.sprites.front_default;
+                                        evolutionChainImg.style.width = pixelSize
+                                        evolutionChainImg.style.height = pixelSize
+                                        figure.appendChild(evolutionChainImg);
+                                        let evolutionName = document.createElement("figcaption");
+                                        evolutionName.innerHTML = evolution;
+                                        figure.appendChild(evolutionName);
+                                        evoDiv.appendChild(figure)
+                                    })
+                            })
+                        }
+                        if (evolutionDetails.evolves_to.length === 1) {
+                            for (i = 0; i < evolutionDetails.evolves_to.length; i++) {
+                                evolutions[i] = evolutionDetails.species.name;
+                                if (evolutionDetails.evolves_to[i].evolves_to.length === 0) {
+                                    evolutions[i+1] = evolutionDetails.evolves_to[i].species.name;
+                                } else if (evolutionDetails.evolves_to[i].evolves_to[i].hasOwnProperty('species')) {
+                                    evolutions[i+1] = evolutionDetails.evolves_to[i].species.name;
+                                    evolutions[i+2] = evolutionDetails.evolves_to[i].evolves_to[i].species.name;
+                                }
+                                deetsToDom("100px");
+                            }
+                        } else if (evolutionDetails.evolves_to.length > 1) {
+                            evolutions[0] = evolutionDetails.species.name;
+                            for (i = 0; i < evolutionDetails.evolves_to.length; i++) {
+                                evolutions[i+1] = evolutionDetails.evolves_to[i].species.name;
+                            }
+                            deetsToDom("75px");
+                        } else {
+                            evoDiv.innerHTML = "";
+                        }
+                    })
             })
     }
-    fetchEvolutions()
+
+    fetchPokemon();
 
 })();
