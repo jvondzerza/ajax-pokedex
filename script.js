bf0b41d8d0b589c978f6d306a5aa06577199be00
jvondzerza <jvondzerza@googlemail.com>
2021-10-15 14:19:07 +0200

    added toggle button for pokemon theme song

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function() {
-
-    let evoDiv = document.getElementById("evo-row");
+    let pokeLogo = document.getElementById("poke-logo");
+    let evoDiv = document.getElementById("evo-div");
     let input = document.getElementById("poke-search");
     let searchButton = document.getElementById("search-button");
     let id = document.getElementById("id");
@@ -11,6 +11,33 @@
     let navNext = document.getElementById("next");
     let randomMoves = ["", "", "", ""];
     let fetchId = 1;
+    let audio = new Audio('poke-nav-sound.mp3');
+    let theme = new Audio('pokemon-theme-fart-version.mp3');
+    let musicButton = document.getElementById("toggle-music");
+    musicButton.innerHTML = "▶️";
+
+    let isPlaying = false;
+    function togglePlay() {
+        if (isPlaying) {
+            theme.pause();
+            musicButton.innerHTML = "▶️";
+            musicButton.style.borderColor = "#ffcb05"
+        } else {
+            theme.play();
+            musicButton.innerHTML = "⏸️";
+            musicButton.style.borderColor = "#3c5aa6"
+        }
+    }
+    theme.onplaying = function() {
+        isPlaying = true;
+    };
+    theme.onpause = function() {
+        isPlaying = false;
+    };
+
+    musicButton.addEventListener("click", function() {
+        togglePlay();
+    })
 
     function inputSanitization() {
         if (input.value.includes(" ")) {
@@ -21,7 +48,12 @@
         }
     }
 
+    pokeLogo.addEventListener("click", function() {
+        window.location.reload();
+    })
+
     searchButton.addEventListener("click", function() {
+        audio.play();
         inputSanitization();
         fetchId = input.value;
         fetchPokemon();
@@ -29,6 +61,7 @@
 
     input.addEventListener("keypress", function (e){
         if (e.key === 'Enter') {
+            audio.play();
             inputSanitization();
             fetchId = input.value;
             fetchPokemon();
@@ -36,6 +69,7 @@
     })
 
     navPrev.addEventListener("click", function() {
+        audio.play();
         fetchId -= 1;
         if (fetchId < 1) {
             fetchId = 898;
@@ -44,6 +78,7 @@
     })
 
     navNext.addEventListener("click", function() {
+        audio.play();
         fetchId += 1;
         if (fetchId > 898) {
             fetchId = 1;
@@ -55,7 +90,6 @@
         fetch(`https://pokeapi.co/api/v2/pokemon/${fetchId}`)
             .then(response => response.json())
             .then(data => {
-                console.log(data);
                 name.innerHTML = data.name;
                 id.innerHTML = data.id;
                 img.src = data.sprites.front_default;
@@ -81,6 +115,7 @@
                 fetchId = parseInt(data.id);
                 fetchEvolutions();
                 randomMoves = ["", "", "", ""];
+                audio.play
             })
     }
 
@@ -103,19 +138,23 @@
                     .then(response => response.json())
                     .then(data => {
                         let evolutionDetails = data.chain;
-                        function deetsToDom(pixelSize) {
+                        console.log(evolutionDetails);
+                        function deetsToDom(pixelSize, fontSize) {
                             evoDiv.innerHTML = "";
                             evolutions.forEach(evolution => {
                                 if (evolution === "wormadam") {
                                     evolution = "wormadam-plant";
                                 }
+                                if (evolution === "urshifu") {
+                                    evolution = "urshifu-single-strike"
+                                }
                                 fetch(`https://pokeapi.co/api/v2/pokemon/${evolution}`)
                                     .then(response => response.json())
                                     .then(data => {
-                                        console.log(data);
                                         let figure = document.createElement("figure");
                                         let evolutionChainImg = document.createElement("img");
                                         figure.setAttribute("class", "evo-fig");
+                                        figure.style.order = data.order;
                                         evolutionChainImg.setAttribute("class", "evo-img");
                                         evolutionChainImg.src = data.sprites.front_default;
                                         evolutionChainImg.style.width = pixelSize
@@ -123,8 +162,14 @@
                                         figure.appendChild(evolutionChainImg);
                                         let evolutionName = document.createElement("figcaption");
                                         evolutionName.innerHTML = evolution;
+                                        evolutionName.style.fontSize = fontSize
                                         figure.appendChild(evolutionName);
                                         evoDiv.appendChild(figure)
+                                        evolutionChainImg.addEventListener("click", function () {
+                                            audio.play();
+                                            fetchId = evolution;
+                                            fetchPokemon();
+                                        })
                                     })
                             })
                         }
@@ -138,14 +183,28 @@
                                     evolutions[i+2] = evolutionDetails.evolves_to[i].evolves_to[i].species.name;
                                 }
                                 console.log(evolutions);
-                                deetsToDom("100px");
                             }
-                        } else if (evolutionDetails.evolves_to.length > 1) {
+                            deetsToDom("100px", "inherit");
+                        } else if (evolutionDetails.evolves_to.length === 2) {
+                            if (evolutionDetails.species.name === "wurmple") {
+                                evolutions[0] = evolutionDetails.species.name;
+                                evolutions[1] = evolutionDetails.evolves_to[0].species.name;
+                                evolutions[2] = evolutionDetails.evolves_to[1].species.name;
+                                evolutions[3] = evolutionDetails.evolves_to[0].evolves_to[0].species.name;
+                                evolutions[4] = evolutionDetails.evolves_to[1].evolves_to[0].species.name;
+                            } else {
+                                evolutions[0] = evolutionDetails.species.name;
+                                for (i = 0; i < evolutionDetails.evolves_to.length; i++) {
+                                    evolutions[i+1] = evolutionDetails.evolves_to[i].species.name;
+                                }
+                            }
+                            deetsToDom("90px", "inherit");
+                        } else if (evolutionDetails.evolves_to.length > 2) {
                             evolutions[0] = evolutionDetails.species.name;
                             for (i = 0; i < evolutionDetails.evolves_to.length; i++) {
                                 evolutions[i+1] = evolutionDetails.evolves_to[i].species.name;
                             }
-                            deetsToDom("75px");
+                            deetsToDom("60px", "x-small");
                         } else {
                             evoDiv.innerHTML = "";
                         }
